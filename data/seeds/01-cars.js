3f4664dde3f5f32520b7a66158085a8878268141
Jiahui Feng <feng_jeffrey@yahoo.com>
2022-07-20 03:19:42 -0400

    MVP complete

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,37 @@
 // STRETCH
+const cars = [
+    {
+        vin: '11111111111111111',
+        make: 'Toyota',
+        model: 'Camry',
+        mileage: 215000,
+        title: 'clean',
+        transmission: 'manual'
+    },
+    {
+        vin: '22222222222222222',
+        make: 'Toyota',
+        model: 'Prius',
+        mileage: 115000,
+        title: 'salvage',
+    },
+    {
+        vin: '33333333333333333',
+        make: 'Ford',
+        model: 'Fiesta',
+        mileage: 21500,
+        title: 'clean',
+    },
+]
+
+// exports.seed = function(knex) {
+//     return knex('cars')
+//     .truncate().then(() => {
+//         return knex('cars').insert(cars)
+//     })
+// }
+
+exports.seed = async function (knex) {
+    await knex('cars').truncate();
+    await knex('cars').insert(cars);
+}
